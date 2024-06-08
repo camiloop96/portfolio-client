f40bf06555978610e0e585214b486589b9987856
Camilo Polania <cpolania@outlook.com>
2024-06-08 12:35:46 -0500

    Documentation added for DataProfile suite test

--- src/modules/DataProfile/DataProfile.test.tsx
+++ src/modules/DataProfile/DataProfile.test.tsx
@@ -1,5 +1,5 @@
 /**
- * @file DataProfile.tsx
+ * @file DataProfile.test.tsx
  * @description Test suite for DataProfile component.
  */
 
@@ -25,9 +25,14 @@ jest.mock("./ContainerFlags/ContainerFlags", () => () => (
   <div data-testid="container-flags">Container Flags</div>
 ));
 
+/**
+ * Test suite for the DataProfile component.
+ */
 describe("DataProfile Component", () => {
+  // Mock user name
   const mockUserName = "John Doe";
 
+  // Mock profile photo details
   const mockProfilePhoto: ProfilePhotoTypes = {
     thumbnail: {
       source: "mockThumbnailSource",
@@ -38,11 +43,14 @@ describe("DataProfile Component", () => {
       alt: "mockFullImageAlt",
     },
   };
+
+  // Mock user data context value
   const mockUserData: UserDataContextTypes = {
-    userName: "John Doe",
+    userName: mockUserName,
     profilePhoto: mockProfilePhoto,
   };
 
+  // Render the component with the mocked context before each test
   beforeEach(() => {
     render(
       <UserDataContext.Provider value={mockUserData}>
@@ -51,26 +59,41 @@ describe("DataProfile Component", () => {
     );
   });
 
+  /**
+   * Test to verify if the profile image is rendered.
+   */
   test("Renders profile image", () => {
     const profileImage = screen.getByTestId("profile-image");
     expect(profileImage).toBeInTheDocument();
   });
 
+  /**
+   * Test to verify if the user name is rendered.
+   */
   test("Renders user name", () => {
     const userNameElement = screen.getByText(mockUserName);
     expect(userNameElement).toBeInTheDocument();
   });
 
+  /**
+   * Test to verify if the links container is rendered.
+   */
   test("Renders links container", () => {
     const linksContainer = screen.getByTestId("links-container");
     expect(linksContainer).toBeInTheDocument();
   });
 
+  /**
+   * Test to verify if the text container is rendered.
+   */
   test("Renders text container", () => {
     const textContainer = screen.getByTestId("text-container");
     expect(textContainer).toBeInTheDocument();
   });
 
+  /**
+   * Test to verify if the container flags are rendered.
+   */
   test("Renders container flags", () => {
     const containerFlags = screen.getByTestId("container-flags");
     expect(containerFlags).toBeInTheDocument();
